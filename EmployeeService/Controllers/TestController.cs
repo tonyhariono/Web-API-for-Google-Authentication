f802887f6bca3a6ef5a07e2dd5797e4941f65fca
tOnY-ROG <tOnY-ROG@DESKTOP-IEITP9C>
2017-05-31 14:20:54 +0700

    Add project files.

---
+++ EmployeeService/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    public class TestController : ApiController
+    {
+        public String Get()
+        {
+            return "Hello from Test Controller";
+        }
+    }
+}
